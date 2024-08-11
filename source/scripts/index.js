7c9bc1a0feb21e02ce69221138b5939f6d0652b9
webDevUs3r <webDevUser@mail.ru>
2024-08-11 14:37:59 +0700

    Стилизует header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const menuButton = document.querySelector('.toggle-button');
+const mainNav = document.querySelector('.main-navigation');
+
+menuButton.addEventListener('click', () => {
+  if (!menuButton.classList.contains('toggle-button--opened')) {
+    menuButton.classList.add('toggle-button--opened');
+    mainNav.classList.add('main-navigation--opened');
+  } else {
+    menuButton.classList.remove('toggle-button--opened');
+    mainNav.classList.remove('main-navigation--opened');
+  }
+});
+
+
