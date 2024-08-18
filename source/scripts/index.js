5b7789ec57ccb586092dcb0f248d346e87eef03d
webDevUs3r <49024850+webDevUs3r@users.noreply.github.com>
2024-08-19 06:51:28 +0700

    Module6-task1 (#12)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -11,3 +11,29 @@ menuButton.addEventListener('click', () => {
     mainNav.classList.remove('main-navigation--opened');
   }
 });
+
+const heroButton = document.querySelector('.hero__button');
+
+const changeModificatorOnButton = () => {
+  const viewport = window.innerWidth;
+
+  if (viewport >= 768) {
+    if (heroButton.classList.contains('button--size-s')) {
+      heroButton.classList.remove('button--size-s');
+    }
+    heroButton.classList.add('button--size-m');
+  } else {
+    if (!heroButton.classList.contains('button--size-s')) {
+      heroButton.classList.add('button--size-s');
+    }
+
+    if (heroButton.classList.contains('button--size-m')) {
+      heroButton.classList.remove('button--size-m');
+    }
+  }
+};
+
+if (heroButton) {
+  changeModificatorOnButton();
+  window.addEventListener('resize', changeModificatorOnButton);
+}
