6f4a654fb785ec9bb3ee2784c8ef4d23ae583c52
webDevUs3r <webDevUser@mail.ru>
2024-09-17 08:08:39 +0300

    Изменения для допуска #2

---
+++ source/scripts/map.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const ymaps = window.ymaps;
+  ymaps.ready(() => {
+    const myMap = new ymaps.Map('map', {
+        center: [59.938679, 30.3230044],
+        zoom: 14,
+        controls: []
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '../icons/stack.svg#icon-pin',
+        iconImageSize: [57, 53],
+        iconImageOffset: [-26, -42]
+      });
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+
+});
